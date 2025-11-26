d77a78cfab42cdc1bf089f178a63909c00e01ab8
rtbnb <eilers.robin@mail.de>
2025-11-26 08:26:47 +0100

    Implemented header encoder and added test

--- src/132/header_encoder.vhd
+++ src/132/header_encoder.vhd
@@ -1,29 +1,43 @@
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+-- File : < FileName >. vhd
+-- Created : < Date >
+-- Author : < Author >
+-- Project Name : < Project Name >
+-- Description : < Short description >
+-- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity header_encoder is
 	Port(
-		clk: in std_logic;
-        In1: in std_logic;
-        In2: in std_logic;
-		result: out std_logic
+        transfer_frame_version_number_i: in std_logic_vector(1 downto 0);
+        spacecraft_id_i: in std_logic_vector(9 downto 0);
+        virtual_channel_id_i: in std_logic_vector(2 downto 0);
+        ocf_flag_i: in std_logic;
+        master_channel_frame_count_i: in std_logic_vector(7 downto 0);
+        virtual_channel_frame_count_i: in std_logic_vector(7 downto 0);
+        transfer_frame_secondary_header_flag_i: in std_logic;
+        snych_flag_i: in std_logic;
+        packet_order_flag_i: in std_logic;
+        segment_length_id_i: in std_logic_vector(1 downto 0);
+        first_header_pointer_i: in std_logic_vector(10 downto 0);
+        header_data_o: out std_logic_vector(47 downto 0)
 	);
 end entity header_encoder;
 
-architecture behavioral of header_encoder is	
-    signal a: std_logic;
-
+architecture behavioral of header_encoder is
 begin
-
-    result <= a;
-
-    process(clk,In1,In2)
-    begin
-        if rising_edge(clk) then
-            a <= In1 and In2; 
-        end if;
-
-        
-    end process;
+    header_data_o(1 downto 0) <= transfer_frame_version_number_i;
+    header_data_o(11 downto 2) <= spacecraft_id_i;
+    header_data_o(14 downto 12) <= virtual_channel_id_i;
+    header_data_o(15) <= ocf_flag_i;
+    header_data_o(23 downto 16) <= master_channel_frame_count_i;
+    header_data_o(31 downto 24) <= virtual_channel_frame_count_i;
+    header_data_o(32) <= transfer_frame_secondary_header_flag_i;
+    header_data_o(33) <= snych_flag_i;
+    header_data_o(34) <= packet_order_flag_i;
+    header_data_o(36 downto 35) <= segment_length_id_i;
+    header_data_o(47 downto 37) <= first_header_pointer_i;
 end architecture behavioral;
--- src/top_level.vhd
+++ src/top_level.vhd
@@ -4,42 +4,51 @@ use ieee.numeric_std.all;
 
 entity top_level is
 	Port(
-		clk: in std_logic;
-		In1: in std_logic;
-        In2: in std_logic;
-        In3: in std_logic;
-		Out1: out std_logic;
-        Out2: out std_logic;
-        Out3: out std_logic
+        transfer_frame_version_number_i: in std_logic_vector(1 downto 0);
+        spacecraft_id_i: in std_logic_vector(9 downto 0);
+        virtual_channel_id_i: in std_logic_vector(2 downto 0);
+        ocf_flag_i: in std_logic;
+        master_channel_frame_count_i: in std_logic_vector(7 downto 0);
+        virtual_channel_frame_count_i: in std_logic_vector(7 downto 0);
+        transfer_frame_secondary_header_flag_i: in std_logic;
+        snych_flag_i: in std_logic;
+        packet_order_flag_i: in std_logic;
+        segment_length_id_i: in std_logic_vector(1 downto 0);
+        first_header_pointer_i: in std_logic_vector(10 downto 0);
+        header_data_o: out std_logic_vector(47 downto 0)
 	);
 end entity top_level;
 
 architecture behavioral of top_level is
     component header_encoder is
         port(
-            clk: in std_logic;
-            In1: in std_logic;
-            In2: in std_logic;
-            result: out std_logic
+            transfer_frame_version_number_i: in std_logic_vector(1 downto 0);
+            spacecraft_id_i: in std_logic_vector(9 downto 0);
+            virtual_channel_id_i: in std_logic_vector(2 downto 0);
+            ocf_flag_i: in std_logic;
+            master_channel_frame_count_i: in std_logic_vector(7 downto 0);
+            virtual_channel_frame_count_i: in std_logic_vector(7 downto 0);
+            transfer_frame_secondary_header_flag_i: in std_logic;
+            snych_flag_i: in std_logic;
+            packet_order_flag_i: in std_logic;
+            segment_length_id_i: in std_logic_vector(1 downto 0);
+            first_header_pointer_i: in std_logic_vector(10 downto 0);
+            header_data_o: out std_logic_vector(47 downto 0)
         );
     end component;
-
-    signal test: std_logic;
 begin
     encoder1: header_encoder port map(
-        clk=>clk,
-        In1=>In1,
-        In2=>In2,
-        result=>test
+        transfer_frame_version_number_i => transfer_frame_version_number_i,
+        spacecraft_id_i => spacecraft_id_i,
+        virtual_channel_id_i => virtual_channel_id_i,
+        ocf_flag_i => ocf_flag_i,
+        master_channel_frame_count_i => master_channel_frame_count_i,
+        virtual_channel_frame_count_i => virtual_channel_frame_count_i,
+        transfer_frame_secondary_header_flag_i => transfer_frame_secondary_header_flag_i,
+        snych_flag_i => snych_flag_i,
+        packet_order_flag_i => packet_order_flag_i,
+        segment_length_id_i => segment_length_id_i,
+        first_header_pointer_i => first_header_pointer_i,
+        header_data_o => header_data_o
     );
-
-    process(clk)
-    begin
-        if rising_edge(clk) then
-            Out3 <= In1 and In3;
-        end if;
-    end process;
-
-    Out2 <= test;
-	Out1 <= In1 and In2;
 end architecture behavioral;
