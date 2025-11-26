2240553ab06fdff9c67e57560fdd21190a3d0e02
Matthiaszockt <54158205+Matthiaszockt@users.noreply.github.com>
2025-11-26 20:40:32 +0100

    8: removed old filesfrom data structure

--- tb/reed_solomon_encoder/generateEncoder_tb_data.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-testDatas = np.linspace(0, 223, 223, dtype=np.uint8)
-
-for testData in testDatas:
-    print('inputByte_r <= x"{:x}";'.format(testData))
-    print("wait for 160 ns;")
