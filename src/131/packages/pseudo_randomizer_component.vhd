23a46682992f6b8316e720d1277f176fee988601
grayandscaled <hannahlindner04@gmail.com>
2025-11-28 12:55:20 +0100

    40: initial commit

---
+++ src/131/packages/pseudo_randomizer_component.vhd
@@ -0,0 +1,21 @@
+----------------------------------------------------------------
+-- File : pseudo_randomizer_component.vhd
+-- Created : 28.11.2025
+-- Author : Hannah Lindner 
+-- Project Name : HW/SW Project TM
+-- Description : generic component for pseudorandomizer, polynomial h(x) = x^17 + x^14 + 1
+----------------------------------------------------------------
+
+library ieee; 
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+
+entity pseudo_randomizer_component is 
+    port();
+end entity pseudo_randomizer_component; 
+
+
+architecture behavioral of pseudo_randomizer_component is 
+    begin 
+    end architecture behavioral;
