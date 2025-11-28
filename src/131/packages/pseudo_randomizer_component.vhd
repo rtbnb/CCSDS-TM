d9a43c89184be75ad64f85a7a3386fca58730667
grayandscaled <hannahlindner04@gmail.com>
2025-11-28 16:46:23 +0100

    40: functional implementation, tested in behav sim

--- src/131/packages/pseudo_randomizer_component.vhd
+++ src/131/packages/pseudo_randomizer_component.vhd
@@ -12,10 +12,55 @@ use ieee.numeric_std.all;
 
 
 entity pseudo_randomizer_component is 
-    port();
+    generic(
+        clock_divider_g : integer := 1
+        );
+    port(
+        -- input ports 
+        clk_i   : in std_logic; 
+        reset_i : in std_logic; 
+        data_i  : in std_logic;
+        -- output ports  
+        data_o  : out std_logic; 
+        done_o  : out std_logic
+    );
 end entity pseudo_randomizer_component; 
 
 
 architecture behavioral of pseudo_randomizer_component is 
+    constant INIT_SEQUENCE          : std_logic_vector(16 downto 0) := "11000111000111000"; 
+    signal randomization_sequence_r : std_logic_vector(16 downto 0) := INIT_SEQUENCE;
+begin 
+
+    -- process to generate the random sequence and XOR it with input data 
+    generate_randomization : process (clk_i, reset_i)
+        variable clock_counter_r    : integer := 0; -- variable for clock division
+        variable new_element_s      : std_logic;
+        variable new_vector_long_s  : std_logic_vector(17 downto 0);
     begin 
-    end architecture behavioral;
+        if reset_i = '0' then 
+            -- reset signals and variable  
+            done_o                      <= '0'; 
+            data_o                      <= '0';
+            randomization_sequence_r    <= INIT_SEQUENCE;
+            clock_counter_r             := 0; 
+            new_element_s               := '0'; 
+            new_vector_long_s           := (others => '0');
+
+        elsif rising_edge(clk_i) then 
+            -- clock division
+            clock_counter_r := clock_counter_r + 1; 
+            if clock_counter_r = clock_divider_g then 
+                -- XOR data 
+                data_o <= data_i XOR randomization_sequence_r(0);
+                -- generate new element + shift vector 
+                new_element_s := randomization_sequence_r(0) XOR randomization_sequence_r(14);
+                new_vector_long_s := new_element_s & randomization_sequence_r;
+                randomization_sequence_r <= new_vector_long_s(17 downto 1); 
+                -- reset clock counter 
+                clock_counter_r := 0;
+            end if; -- clock divider 
+        end if; -- rising edge logic 
+    end process generate_randomization;
+
+end architecture behavioral;
