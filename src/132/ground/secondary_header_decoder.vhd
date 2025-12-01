b22d9ebcaaec9562db8fb203282876ffe77cb1fe
Nico Tunkowski <nico.tunkowski@gmail.com>
2025-12-01 22:14:45 +0100

    11: Implemented decode logic

--- src/132/ground/secondary_header_decoder.vhd
+++ src/132/ground/secondary_header_decoder.vhd
@@ -16,10 +16,13 @@ entity secondary_header_decoder is
     );
     port(
         enable_i: in std_logic;
-        clk_i: in std_logic; -- data is read at falling edge of clk_i
+        clk_i: in std_logic; -- data is read on falling edge of clk_i
         data_i: in std_logic_vector(7 downto 0);
 
-        secondary_header_data_o: out std_logic_vector(7 downto 0 ) := (others => '0'); -- data can be read at falling edge of clk_i
+        reset_i: in std_logic; -- active low
+
+        secondary_header_data_o: out std_logic_vector(7 downto 0 ) := (others => '0'); -- data can be read on falling edge of get_next_data_octet_i
+        get_next_data_octet_i: in std_logic; -- data is valid on falling edge of get_next_data_octet_i
         secondary_header_fully_read_o: out std_logic := '0';
         version_o: out std_logic_vector(1 downto 0);
         length_o: out std_logic_vector(5 downto 0)
@@ -27,6 +30,42 @@ entity secondary_header_decoder is
 end entity secondary_header_decoder;
 
 architecture behavioral of secondary_header_decoder is
+    signal secondary_header_r : std_logic_vector(((secondary_header_data_field_width_octets_g + 1) * 8) - 1 downto 0) := (others => '0');
 begin
+
+    -- output secondary header id
+    version_o <= secondary_header_r(1 downto 0);
+    length_o <= secondary_header_r(7 downto 2);
+
+    read_data: process(clk_i) is
+        variable data_octet_counter: integer := 0;
+    begin
+        if falling_edge(clk_i) and enable_i = '1' then
+            if data_octet_counter = secondary_header_data_field_width_octets_g + 1 then
+                data_octet_counter := 0;
+                secondary_header_r(((data_octet_counter + 1)  * 8) - 1 downto data_octet_counter  * 8) <= data_i;
+            else 
+                data_octet_counter := data_octet_counter + 1;
+                secondary_header_r(((data_octet_counter + 1)  * 8) - 1 downto data_octet_counter  * 8) <= data_i;
+            end if;
+        end if;
+    end process read_data;
+
+    -- output data field logic
+    output_data_field: process(get_next_data_octet_i) is
+        variable data_octet_counter: integer := 0;
+    begin
+        if rising_edge(get_next_data_octet_i) then
+            if data_octet_counter = secondary_header_data_field_width_octets_g then
+                secondary_header_fully_read_o <= '1';
+                secondary_header_data_o <= secondary_header_r( ((data_octet_counter + 2) * 8) - 1 downto (data_octet_counter + 1) * 8);
+                data_octet_counter := 0;
+            else
+                secondary_header_data_o <= secondary_header_r( ((data_octet_counter + 2) * 8) - 1 downto (data_octet_counter + 1) * 8);
+                data_octet_counter := data_octet_counter + 1;
+                secondary_header_fully_read_o <= '0';
+            end if;
+        end if;
+    end process output_data_field;
 
 end architecture behavioral;
