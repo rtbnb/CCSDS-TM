6b74616af089e47592368b5d28b03d9be2c64550
Lukas Reil <lukas_reil0305@gmx.net>
2026-04-23 18:33:14 +0200

    49: Implemented Width Conversion FIFO with TBs

---
+++ src/131/packages/width_conversion_fifo.vhd
@@ -0,0 +1,129 @@
+----------------------------------------------------------------
+-- File : width_conversion_fifo.vhd
+-- Created : 23.04.2026
+-- Author : Lukas Reil 
+-- Project Name : HW/SW Project TM
+-- Description : FIFO to change data width between two clock domains. The FIFO can be configured to have an input data width that is either wider, narrower or the same as the output data width. The FIFO depth is defined in terms of the number of output data words it can hold.
+----------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity width_conversion_fifo is
+    generic (
+        input_data_width_g : integer := 8;
+        output_data_width_g : integer := 8;
+        depth_g      : integer := 16  -- How many entries the FIFO can hold (the width is which ever is wider)
+    );
+    port (
+        -- Write Interface
+        wr_clk_i   : in  std_logic;
+        wr_en_i    : in  std_logic;
+        wr_data_i  : in  std_logic_vector(input_data_width_g-1 downto 0);
+        full_o     : out std_logic;
+
+        -- Read Interface
+        rd_clk_i   : in  std_logic;
+        rd_en_i    : in  std_logic;
+        rd_data_o  : out std_logic_vector(output_data_width_g-1 downto 0);
+        empty_o    : out std_logic
+    );
+end entity width_conversion_fifo;
+
+architecture behavioral of width_conversion_fifo is
+
+    type fifo_mode_t is (SAME, INPUT_WIDER, OUTPUT_WIDER);
+    constant FIFO_MODE : fifo_mode_t := 
+                SAME            when input_data_width_g = output_data_width_g else
+                INPUT_WIDER     when input_data_width_g > output_data_width_g else
+                OUTPUT_WIDER;
+    
+    constant RATIO : integer := 
+                1               when FIFO_MODE = SAME else
+                input_data_width_g / output_data_width_g when FIFO_MODE = INPUT_WIDER else
+                output_data_width_g / input_data_width_g;
+    
+    constant FIFO_MEM_WIDTH : integer := 
+                input_data_width_g  when FIFO_MODE = SAME or FIFO_MODE = INPUT_WIDER else
+                output_data_width_g;
+    
+    type fifo_mem_t is array (0 to depth_g-1) of std_logic_vector(FIFO_MEM_WIDTH-1 downto 0);
+    signal fifo_mem_r : fifo_mem_t := (others => (others => '0'));
+
+    signal wr_ptr_r : integer range 0 to depth_g-1 := 0;
+    signal rd_ptr_r : integer range 0 to depth_g-1 := 0;
+    signal empty_s : std_logic;
+    signal full_s  : std_logic;
+
+    signal fifo_data_out_r : std_logic_vector(output_data_width_g-1 downto 0) := (others => '0');
+
+    
+begin
+
+    assert (
+        (input_data_width_g > 0) and (output_data_width_g > 0) and (depth_g > 0)
+    ) 
+    report "Data widths and depth must be positive integers" severity failure;
+
+    assert (
+        ((input_data_width_g mod output_data_width_g) = 0 and (input_data_width_g > output_data_width_g))
+        or 
+        ((input_data_width_g mod input_data_width_g) = 0 and (output_data_width_g > input_data_width_g))
+        or
+        (input_data_width_g = output_data_width_g)
+    )
+    report "Input and output data widths must be multiples of each other" severity failure;
+
+    empty_s <= '1' when wr_ptr_r = rd_ptr_r else '0';
+    full_s  <= '1' when ((wr_ptr_r + 1) mod depth_g) = rd_ptr_r else '0';
+
+    empty_o <= empty_s;
+    full_o  <= full_s;
+
+    process(wr_clk_i)
+        variable expander_counter_v : integer range 0 to RATIO-1 := RATIO - 1;
+    begin
+        if rising_edge(wr_clk_i) then
+            if wr_en_i = '1' and full_s = '0' then
+                if FIFO_MODE = SAME or FIFO_MODE = INPUT_WIDER then
+                    fifo_mem_r(wr_ptr_r) <= wr_data_i;
+                    wr_ptr_r <= (wr_ptr_r + 1) mod depth_g;
+                elsif FIFO_MODE = OUTPUT_WIDER then
+                    fifo_mem_r(wr_ptr_r)(
+                        (expander_counter_v + 1) * input_data_width_g - 1 downto expander_counter_v * input_data_width_g
+                    ) <= wr_data_i;
+                    expander_counter_v := (expander_counter_v - 1) mod RATIO;
+                    if expander_counter_v = RATIO - 1 then
+                        wr_ptr_r <= (wr_ptr_r + 1) mod depth_g;
+                    end if;
+                end if;
+            end if;
+        end if;
+    end process;
+
+    process(rd_clk_i)
+        variable expander_counter_v : integer range 0 to RATIO-1 := RATIO - 1;  
+    begin
+        if rising_edge(rd_clk_i) then
+            if rd_en_i = '1' and empty_s = '0' then
+                
+                if FIFO_MODE = SAME or FIFO_MODE = OUTPUT_WIDER then
+                    fifo_data_out_r <= fifo_mem_r(rd_ptr_r);
+                    rd_ptr_r <= (rd_ptr_r + 1) mod depth_g;
+                elsif FIFO_MODE = INPUT_WIDER then
+                    fifo_data_out_r <= fifo_mem_r(rd_ptr_r)(
+                        (expander_counter_v + 1) * output_data_width_g - 1 downto expander_counter_v * output_data_width_g
+                    );
+                    expander_counter_v := (expander_counter_v - 1) mod RATIO;
+                    if expander_counter_v = RATIO - 1 then
+                        rd_ptr_r <= (rd_ptr_r + 1) mod depth_g;
+                    end if;
+                end if;
+
+            end if;
+        end if;
+    end process;
+
+    rd_data_o <= fifo_data_out_r;
+    
+end architecture behavioral;
--- tb/131/packages/width_conversion_fifo_tb_2.vhd
+++ tb/131/packages/width_conversion_fifo_tb_2.vhd
@@ -1,9 +1,9 @@
 ----------------------------------------------------------------
--- File : synchronization_fifo_tb.vhd
--- Created : 29.11.2025
+-- File : width_conversion_fifo_tb_2.vhd
+-- Created : 23.04.2026
 -- Author : Lukas Reil 
 -- Project Name : HW/SW Project TM
--- Description : Testbench for a generic FIFO to synchronize between two clock domains
+-- Description : Testbench for a generic FIFO to synchronize between two clock domains with width conversion
 ----------------------------------------------------------------
 
 library ieee;
@@ -11,53 +11,56 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 
-entity synchronization_fifo_tb is
-end entity synchronization_fifo_tb;
+entity width_conversion_fifo_tb_2 is
+end entity width_conversion_fifo_tb_2;
 
-architecture behavioral of synchronization_fifo_tb is
+architecture behavioral of width_conversion_fifo_tb_2 is
 
-    constant DATA_WIDTH : integer := 8;
+    constant INPUT_DATA_WIDTH : integer := 8;
+    constant OUTPUT_DATA_WIDTH : integer := 16;
     constant DEPTH      : integer := 16;
 
     signal wr_clk_r   : std_logic := '0';
     signal wr_en_r    : std_logic := '0';
-    signal wr_data_r  : std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
+    signal wr_data_r  : std_logic_vector(INPUT_DATA_WIDTH-1 downto 0) := (others => '0');
     signal full_s     : std_logic;
 
     signal rd_clk_r   : std_logic := '0';
     signal rd_en_r    : std_logic := '0';
-    signal rd_data_s  : std_logic_vector(DATA_WIDTH-1 downto 0);
+    signal rd_data_s  : std_logic_vector(OUTPUT_DATA_WIDTH-1 downto 0);
     signal empty_s    : std_logic;
 
     constant RD_CLK_PERIOD : time := 10 ns;
     constant WR_CLK_PERIOD : time := 7 ns;
 
-    component synchronization_fifo is
+    component width_conversion_fifo is
         generic (
-            data_width_g : integer := DATA_WIDTH;
+            input_data_width_g : integer := INPUT_DATA_WIDTH;
+            output_data_width_g : integer := OUTPUT_DATA_WIDTH;
             depth_g      : integer := DEPTH
         );
         port (
             -- Write Interface
             wr_clk_i   : in  std_logic;
             wr_en_i    : in  std_logic;
-            wr_data_i  : in  std_logic_vector(data_width_g-1 downto 0);
+            wr_data_i  : in  std_logic_vector(input_data_width_g-1 downto 0);
             full_o     : out std_logic;
 
             -- Read Interface
             rd_clk_i   : in  std_logic;
             rd_en_i    : in  std_logic;
-            rd_data_o  : out std_logic_vector(data_width_g-1 downto 0);
+            rd_data_o  : out std_logic_vector(output_data_width_g-1 downto 0);
             empty_o    : out std_logic
         );
-    end component synchronization_fifo;
+    end component width_conversion_fifo;
 
 
 begin
 
-    fifo : synchronization_fifo
+    fifo : width_conversion_fifo
         generic map (
-            data_width_g => DATA_WIDTH,
+            input_data_width_g => INPUT_DATA_WIDTH,
+            output_data_width_g => OUTPUT_DATA_WIDTH,
             depth_g      => DEPTH
         )
         port map (
@@ -106,7 +109,7 @@ begin
         wait for WR_CLK_PERIOD * 5;
         for i in 0 to 15 loop
             wr_en_r <= '1';
-            wr_data_r <= std_logic_vector(to_unsigned(i, DATA_WIDTH));
+            wr_data_r <= std_logic_vector(to_unsigned(i, INPUT_DATA_WIDTH));
             wait for WR_CLK_PERIOD;
         end loop;
         wr_en_r <= '0';
