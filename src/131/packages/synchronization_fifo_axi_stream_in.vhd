e6dd3a5ac604061cc25324d037adbe79f64b1b4b
Lukas Reil <lukas.reil0305@gmx.net>
2026-05-17 15:30:36 +0200

    65: Updated generic names to comply with new VHDL Standard

--- src/131/packages/synchronization_fifo_axi_stream_in.vhd
+++ src/131/packages/synchronization_fifo_axi_stream_in.vhd
@@ -12,21 +12,21 @@ use ieee.std_logic_1164.all;
 
 entity synchronization_fifo_axi_stream_in is
     generic (
-        data_width_g : integer := 8;
-        depth_g      : integer := 16
+        DATA_WIDTH : integer := 8;
+        DEPTH      : integer := 16
     );
     port (
         -- Input Interface
         s_axis_aclk : in  std_logic;
         s_axis_aresetn : in  std_logic;
         s_axis_tvalid : in  std_logic;
-        s_axis_tdata  : in  std_logic_vector(data_width_g-1 downto 0);
+        s_axis_tdata  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
         s_axis_tready : out std_logic;
 
         -- Read Interface
         rd_clk_i   : in  std_logic;
         rd_en_i    : in  std_logic;
-        rd_data_o  : out std_logic_vector(data_width_g-1 downto 0);
+        rd_data_o  : out std_logic_vector(DATA_WIDTH-1 downto 0);
         empty_o    : out std_logic
     );
 end entity synchronization_fifo_axi_stream_in;
@@ -36,19 +36,19 @@ use ieee.std_logic_1164.all;
 
 architecture behavioral of synchronization_fifo_axi_stream_in is
 
-    type fifo_mem_t is array (0 to depth_g-1) of std_logic_vector(data_width_g-1 downto 0);
+    type fifo_mem_t is array (0 to DEPTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
     signal fifo_mem_r : fifo_mem_t := (others => (others => '0'));
 
-    signal wr_ptr_r : integer range 0 to depth_g-1 := 0;
-    signal rd_ptr_r : integer range 0 to depth_g-1 := 0;
+    signal wr_ptr_r : integer range 0 to DEPTH-1 := 0;
+    signal rd_ptr_r : integer range 0 to DEPTH-1 := 0;
     signal empty_s : std_logic;
     signal full_s  : std_logic;
 
-    signal fifo_data_out_r : std_logic_vector(data_width_g-1 downto 0) := (others => '0');
+    signal fifo_data_out_r : std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
 begin
 
     empty_s <= '1' when wr_ptr_r = rd_ptr_r else '0';
-    full_s  <= '1' when ((wr_ptr_r + 1) mod depth_g) = rd_ptr_r else '0';
+    full_s  <= '1' when ((wr_ptr_r + 1) mod DEPTH) = rd_ptr_r else '0';
 
     empty_o <= empty_s;
     s_axis_tready <= not full_s;
@@ -60,7 +60,7 @@ begin
         elsif rising_edge(s_axis_aclk) then
             if s_axis_tvalid = '1' and full_s = '0' then
                 fifo_mem_r(wr_ptr_r) <= s_axis_tdata;
-                wr_ptr_r <= (wr_ptr_r + 1) mod depth_g;
+                wr_ptr_r <= (wr_ptr_r + 1) mod DEPTH;
             end if;
         end if;
     end process;
@@ -70,7 +70,7 @@ begin
         if rising_edge(rd_clk_i) then
             if rd_en_i = '1' and empty_s = '0' then
                 fifo_data_out_r <= fifo_mem_r(rd_ptr_r);
-                rd_ptr_r <= (rd_ptr_r + 1) mod depth_g;
+                rd_ptr_r <= (rd_ptr_r + 1) mod DEPTH;
             end if;
         end if;
     end process;
--- src/131/packages/synchronization_fifo_axi_stream_out.vhd
+++ src/131/packages/synchronization_fifo_axi_stream_out.vhd
@@ -12,40 +12,40 @@ use ieee.std_logic_1164.all;
 
 entity synchronization_fifo_axi_stream_out is
     generic (
-        data_width_g : integer := 8;
-        depth_g      : integer := 16
+        DATA_WIDTH : integer := 8;
+        DEPTH      : integer := 16
     );
     port (
         -- Input Interface
         wr_clk_i   : in  std_logic;
         wr_en_i    : in  std_logic;
-        wr_data_i  : in  std_logic_vector(data_width_g-1 downto 0);
+        wr_data_i  : in  std_logic_vector(DATA_WIDTH-1 downto 0);
         full_o     : out std_logic;
 
         -- Output Interface
         m_axis_aclk : in  std_logic;
         m_axis_aresetn : in  std_logic;
         m_axis_tvalid : out std_logic;
-        m_axis_tdata  : out std_logic_vector(data_width_g-1 downto 0);
+        m_axis_tdata  : out std_logic_vector(DATA_WIDTH-1 downto 0);
         m_axis_tready : in  std_logic;
     );
 end entity synchronization_fifo_axi_stream_out;
 
 architecture behavioral of synchronization_fifo_axi_stream_out is
 
-    type fifo_mem_t is array (0 to depth_g-1) of std_logic_vector(data_width_g-1 downto 0);
+    type fifo_mem_t is array (0 to DEPTH-1) of std_logic_vector(DATA_WIDTH-1 downto 0);
     signal fifo_mem_r : fifo_mem_t := (others => (others => '0'));
 
-    signal wr_ptr_r : integer range 0 to depth_g-1 := 0;
-    signal rd_ptr_r : integer range 0 to depth_g-1 := 0;
+    signal wr_ptr_r : integer range 0 to DEPTH-1 := 0;
+    signal rd_ptr_r : integer range 0 to DEPTH-1 := 0;
     signal empty_s : std_logic;
     signal full_s  : std_logic;
 
-    signal fifo_data_out_r : std_logic_vector(data_width_g-1 downto 0) := (others => '0');
+    signal fifo_data_out_r : std_logic_vector(DATA_WIDTH-1 downto 0) := (others => '0');
 begin
 
     empty_s <= '1' when wr_ptr_r = rd_ptr_r else '0';
-    full_s  <= '1' when ((wr_ptr_r + 1) mod depth_g) = rd_ptr_r else '0';
+    full_s  <= '1' when ((wr_ptr_r + 1) mod DEPTH) = rd_ptr_r else '0';
 
     full_o  <= full_s;
 
@@ -54,7 +54,7 @@ begin
         if rising_edge(wr_clk_i) then
             if wr_en_i = '1' and full_s = '0' then
                 fifo_mem_r(wr_ptr_r) <= wr_data_i;
-                wr_ptr_r <= (wr_ptr_r + 1) mod depth_g;
+                wr_ptr_r <= (wr_ptr_r + 1) mod DEPTH;
             end if;
         end if;
     end process;
@@ -67,7 +67,7 @@ begin
         elsif rising_edge(m_axis_aclk) then
             if m_axis_tready = '1' and empty_s = '0' then
                 fifo_data_out_r <= fifo_mem_r(rd_ptr_r);
-                rd_ptr_r <= (rd_ptr_r + 1) mod depth_g;
+                rd_ptr_r <= (rd_ptr_r + 1) mod DEPTH;
                 m_axis_tvalid <= '1';
             else
                 m_axis_tvalid <= '0';
