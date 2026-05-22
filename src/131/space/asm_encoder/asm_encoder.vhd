b3ef60595f5edfdded79ca1340a469292adf77f2
grayandscaled <hannahlindner04@gmail.com>
2026-05-22 23:44:19 +0200

    78: added axi to asm encoder ! needs to be tested

--- src/131/space/asm_encoder/asm_encoder.vhd
+++ src/131/space/asm_encoder/asm_encoder.vhd
@@ -14,59 +14,60 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity asm_encoder is
     generic(
-        clock_divider_g : integer := 1
-        );
+        asm_pattern_g   : std_logic_vector(31 downto 0) := x"1ACFFC1D"
+    );
     port (
         -- input 
         clk_i           : in std_logic; 
         reset_i         : in std_logic;
-        data_i          : in std_logic;
-        encoder_done_i  : in std_logic; 
-        data_valid_i    : in std_logic;  
-        -- output  
-        data_o          : out std_logic;
-        data_valid_o    : out std_logic
+        
+        -- axi stream input 
+        s_axi_tvalid    : in std_logic;
+        s_axi_tready    : out std_logic; 
+        s_axi_tdata     : in std_logic;
+        s_axi_tlast     : in std_logic; 
+
+        -- axi stream output
+        m_axi_tvalid    : out std_logic; 
+        m_axi_tready    : in std_logic;
+        m_axi_tdata     : out std_logic;
+        m_axi_tlast     : out std_logic
     ); 
 end asm_encoder;
 
 architecture behavioral of asm_encoder is
 
-constant ASM_PATTERN    : std_logic_vector(31 downto 0) := x"1ACFFC1D";
 signal counter_r        : integer range 0 to 32 := 0;
+signal generate_asm     : std_logic := '0';
 
 begin
 
 P1: process(clk_i,reset_i)
-    variable clock_counter_r    : integer := 0; -- variable for clock division
-   
+ 
 begin 
     if reset_i = '0' then 
         -- reset all variables and signals 
         counter_r       <= 0;
-        data_o          <= '0';
-        data_valid_o    <= '0';
-        clock_counter_r := 0;
+        m_axi_tdata     <= '0';
+        m_axi_tvalid    <= '0';
         
     elsif rising_edge(clk_i) then
-        -- increase clock counter 
-        clock_counter_r := clock_counter_r + 1; 
-        if clock_counter_r = clock_divider_g then 
-            data_valid_o <= '0'; -- reset clock
-            -- check if input data is valid 
-            if data_valid_i ='1' then
-                if encoder_done_i = '1' then 
-                    -- include asm if pseudorandomizer done
-                    data_o <= ASM_PATTERN(31-counter_r);
-                    counter_r <= counter_r + 1;
-                    data_valid_o <= '1';
-                else
-                    -- otherwise forward data 
-                    counter_r <= 0; 
-                    data_o <= data_i; 
-                    data_valid_o <= '1';
-                end if; 
-           end if;
-        clock_counter_r := 0; -- reset clock counter 
+        if s_axi_tvalid = '1' and m_axi_tready = '1' then 
+            m_axi_tdata <= s_axi_tdata;
+            if s_axi_tlast = '1' then 
+                generate_asm <= '1';
+                s_axi_tready <= '0';
+            end if; 
+        
+        elsif generate_asm = '1' then 
+            m_axi_tdata <= asm_pattern_g(31-counter_r);
+            counter_r <= counter_r + 1;
+            if counter_r = 31 then 
+                counter_r <= 0;
+                generate_asm <= '0';
+            end if; 
+        else 
+            m_axi_tvalid <= '0';
         end if;
     end if;
     
