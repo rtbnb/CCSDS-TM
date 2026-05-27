3414c710c7ccb76bf6fdeb16d559c16942194ce7
Robin Eilers <eilers.robin@mail.de>
2026-05-27 18:02:35 +0200

    83: beginning to implement space packet inspection

--- src/132/space/virtual_channel_buffer.vhd
+++ src/132/space/virtual_channel_buffer.vhd
@@ -25,17 +25,31 @@ entity virtual_channel_buffer is
         frame_ready_o: out std_logic;
         data_out_en_i: in std_logic;
         data_o: out std_logic_vector(7 downto 0) := (others => '0');
-        virtual_channel_frame_count_o: out std_logic_vector(7 downto 0)
-
-
+        virtual_channel_frame_count_o: out std_logic_vector(7 downto 0);
+        first_header_pointer: out std_logic_vector(10 downto 0)
 	);
 end entity virtual_channel_buffer;
 
 architecture behavioral of virtual_channel_buffer is
     constant BUFFER_SIZE: integer := 2040; -- 2040 bytes maximum TRANSFER FRAME DATA FIELD (ref CCSDS 132.0-B-3 Figure 4.1) and SPC-REQ-7
+    constant SPACE_PACKET_PRIMARY_HEADER_SIZE: integer := 6; -- size of space packet header based on the CCSDS 133 specification
+    constant SPACE_PACKET_MAX_DATA_SIZE: integer := 256; -- The maximum defined size of the space package data field as defined by SeeSat
+    constant SPACE_PACKET_MAX_SIZE: integer := SPACE_PACKET_PRIMARY_HEADER_SIZE + SPACE_PACKET_MAX_DATA_SIZE;
+    
     type buffer_mem_t is array (0 to BUFFER_SIZE -1) of std_logic_vector(7 downto 0);
+    type space_package_mem_t is array (0 to SPACE_PACKET_MAX_SIZE -1) of std_logic_vector(7 downto 0);
+    
     signal data_buffer_r : buffer_mem_t := (others => (others => '0'));
+    signal space_packet_buffer_r: space_package_mem_t := (others => (others => '0'));
 
+    type space_package_decoding_state_machine_t IS (RESET, WRITE, INSPECT, READ);
+    signal space_packet_decoding_state_r: space_package_decoding_state_machine_t := RESET; 
+    
+    signal space_packet_write_ptr_r: integer range 0 to SPACE_PACKET_MAX_DATA_SIZE -1 := 0;
+    signal space_packet_read_ptr_r: integer range 0 to SPACE_PACKET_MAX_DATA_SIZE -1 := 0;
+    signal remaining_octets_in_space_package_r: integer range 0 to SPACE_PACKET_MAX_DATA_SIZE -1 := 0;
+    signal space_packet_data_length: integer range 0 to SPACE_PACKET_MAX_DATA_SIZE -1 := 0;
+    
     signal write_ptr_r: integer range 0 to BUFFER_SIZE -1 := 0;
     signal read_ptr_r: integer range 0 to BUFFER_SIZE -1 := 0;
     
@@ -60,6 +74,49 @@ begin
         end if;
         
     end process preload_switch;
+    
+    space_package_ingestion: process(clk_i)
+    begin
+        if rising_edge(clk_i) and reset_i = '1' then
+            if space_packet_decoding_state_r = RESET then
+                space_packet_write_ptr_r <= 0;
+                space_packet_read_ptr_r <= 0;
+                remaining_octets_in_space_package_r <= 0; 
+                
+            
+            elsif space_packet_decoding_state_r = WRITE and data_valid_i = '1' then
+                
+                space_packet_buffer_r(space_packet_write_ptr_r) <= data_i;    
+                space_packet_write_ptr_r <= space_packet_write_ptr_r + 1;
+                
+                if space_packet_write_ptr_r = SPACE_PACKET_PRIMARY_HEADER_SIZE then
+                
+                end if;
+                
+                if space_packet_write_ptr_r = SPACE_PACKET_MAX_DATA_SIZE -1 then
+                    space_packet_write_ptr_r <= 0;
+                    space_packet_decoding_state_r <= INSPECT; 
+                end if;
+                
+            elsif space_packet_decoding_state_r = INSPECT then            
+                
+                
+            elsif space_packet_decoding_state_r = READ then
+                
+            
+            
+            end if;
+        elsif reset_i = '0' then
+            space_packet_write_ptr_r <= 0;
+            space_packet_read_ptr_r <= 0;
+            remaining_octets_in_space_package_r <= 0;         
+            space_packet_decoding_state_r <= RESET;
+        end if;
+        
+        
+        
+    end process space_package_ingestion;
+    
     
     data_handling: process(clk_i) 
     begin
