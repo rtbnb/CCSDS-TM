9ff5cca35369eaa8aff29abe6be200394b3b8333
Nico Tunkowski <nico.tunkowski@gmail.com>
2026-05-30 13:26:36 +0200

    81: Updated Block diagram

--- src/system_integration.tcl
+++ src/system_integration.tcl
@@ -320,17 +320,17 @@ proc create_root_design { parentCell } {
   connect_bd_net -net ccsds_131_space_0_read_data_fifo_o  [get_bd_pins ccsds_131_space_0/read_data_fifo_o] \
   [get_bd_pins synchronization_fifo_0/rd_en_i]
   connect_bd_net -net clk_i_0_1  [get_bd_ports clk_i_0] \
-  [get_bd_pins synchronization_fifo_0/rd_clk_i] \
   [get_bd_pins ccsds_131_space_0/clk_i] \
-  [get_bd_pins virtual_channel_buff_0/clk_i] \
+  [get_bd_pins synchronization_fifo_0/wr_clk_i] \
+  [get_bd_pins synchronization_fifo_0/rd_clk_i] \
   [get_bd_pins transfer_frame_encod_0/clk_i] \
-  [get_bd_pins synchronization_fifo_0/wr_clk_i]
+  [get_bd_pins virtual_channel_buff_0/clk_i]
   connect_bd_net -net clk_i_1_1  [get_bd_ports ground_clk_i_1] \
   [get_bd_pins ccsds_131_ground_0/clk_i] \
+  [get_bd_pins empty_to_valid_0/clk_i] \
   [get_bd_pins synchronization_fifo_1/wr_clk_i] \
-  [get_bd_pins decoder_buffer_and_s_0/clk_i] \
   [get_bd_pins synchronization_fifo_1/rd_clk_i] \
-  [get_bd_pins empty_to_valid_0/clk_i]
+  [get_bd_pins decoder_buffer_and_s_0/clk_i]
   connect_bd_net -net data_i_0_1  [get_bd_ports data_i_0] \
   [get_bd_pins virtual_channel_buff_0/data_i]
   connect_bd_net -net data_valid_i_0_1  [get_bd_ports data_valid_i_0] \
@@ -346,8 +346,9 @@ proc create_root_design { parentCell } {
   connect_bd_net -net reset_i_0_1  [get_bd_ports reset_i_0] \
   [get_bd_pins ccsds_131_ground_0/reset_i] \
   [get_bd_pins ccsds_131_space_0/reset_i] \
+  [get_bd_pins transfer_frame_encod_0/reset_i] \
   [get_bd_pins virtual_channel_buff_0/reset_i] \
-  [get_bd_pins transfer_frame_encod_0/reset_i]
+  [get_bd_pins decoder_buffer_and_s_0/reset_i]
   connect_bd_net -net spacecraft_id_i_0_1  [get_bd_ports spacecraft_id_i_0] \
   [get_bd_pins transfer_frame_encod_0/spacecraft_id_i]
   connect_bd_net -net synchronization_fifo_0_empty_o  [get_bd_pins synchronization_fifo_0/empty_o] \
