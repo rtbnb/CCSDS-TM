a2a9b6b4a7b068ac9f4bccfaa276b1bb4ec29db5
grayandscaled <hannahlindner04@gmail.com>
2026-05-31 10:39:22 +0200

    78: added axi stream to pseudo_randomizer

--- src/131/packages/pseudo_randomizer.vhd
+++ src/131/packages/pseudo_randomizer.vhd
@@ -11,7 +11,7 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 
-entity pseudo_randomizer is 
+entity pseudo_randomizer_entity is 
     port(
         -- input ports 
         clk_i           : in std_logic; 
@@ -28,12 +28,19 @@ entity pseudo_randomizer is
         m_axi_tdata     : out std_logic; 
         m_axi_tlast     : out std_logic
     );
-end entity pseudo_randomizer; 
+end entity pseudo_randomizer_entity; 
 
 
-architecture behavioral of pseudo_randomizer is 
+architecture behavioral of pseudo_randomizer_entity is 
     constant INIT_SEQUENCE          : std_logic_vector(16 downto 0) := "11000111000111000"; 
     signal randomization_sequence_r : std_logic_vector(16 downto 0) := INIT_SEQUENCE;
+
+    signal s_axi_datavalid : std_logic := '0';
+    signal m_axi_datavalid : std_logic := '0';
+    
+    signal m_tvalid : std_logic := '0';
+    signal s_tready : std_logic := '0';
+
 begin 
 
     -- process to generate the random sequence and XOR it with input data 
@@ -42,33 +49,45 @@ begin
         variable new_vector_long_s  : std_logic_vector(17 downto 0);
     begin 
         if reset_i = '0' then 
-            -- reset signals 
-            -- check if encoder done, set data valid accordingly 
-            m_axi_tvalid                <= '0';
-            m_axi_tlast                 <= '0';
-            m_axi_tdata                 <= '0';
+            -- reset signals  
+            m_tvalid                    <= '0';
             randomization_sequence_r    <= INIT_SEQUENCE;
             -- reset variables  
             new_element_s               := '0'; 
             new_vector_long_s           := (others => '0');
 
         elsif rising_edge(clk_i) then
-            -- XOR data 
-            m_axi_tdata     <= s_axi_tdata XOR randomization_sequence_r(0);
-            m_axi_tvalid    <= '1';
-            m_axi_tlast     <= '0';
-            
-            -- generate new element + shift vector 
-            new_element_s := randomization_sequence_r(0) XOR randomization_sequence_r(14);
-            new_vector_long_s := new_element_s & randomization_sequence_r;
-            randomization_sequence_r <= new_vector_long_s(17 downto 1); 
-            
-            if s_axi_tlast = '1' then 
-                m_axi_tlast <= '1'; 
-                randomization_sequence_r <= INIT_SEQUENCE;
-            end if; 
-            
+        
+          -- reset data valid flags 
+            if m_axi_datavalid = '1' then
+                m_tvalid        <= '0';
+                m_axi_tlast     <= '0';
+                
+            elsif s_axi_datavalid = '1' then 
+                -- XOR data 
+                m_axi_tdata     <= s_axi_tdata XOR randomization_sequence_r(0);
+                m_tvalid        <= '1';
+                
+                -- generate new element + shift vector 
+                new_element_s := randomization_sequence_r(0) XOR randomization_sequence_r(14);
+                new_vector_long_s := new_element_s & randomization_sequence_r;
+                randomization_sequence_r <= new_vector_long_s(17 downto 1); 
+                
+                if s_axi_tlast = '1' then 
+                    m_axi_tlast <= '1'; 
+                    randomization_sequence_r <= INIT_SEQUENCE;
+                end if;
+            end if;
         end if; -- rising edge logic 
     end process generate_randomization;
+
+    -- asynchronous assignments 
+    
+    s_axi_datavalid     <= s_tready and s_axi_tvalid;
+    m_axi_datavalid     <= m_axi_tready and m_tvalid;
+    
+    s_tready            <= m_axi_tready;
+    m_axi_tvalid        <= m_tvalid;
+    s_axi_tready        <= s_tready;
 
 end architecture behavioral;
