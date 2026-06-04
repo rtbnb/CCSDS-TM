3a87c24d463405a9d39b796cfc2c13631f7c8e6a
Nico Tunkowski <nico.tunkowski@gmail.com>
2026-06-04 09:37:50 +0200

    104: Added virtual and master channel demultiplexing entities

---
+++ src/132/ground/virtual_channel_demultiplexer.vhd
@@ -0,0 +1,57 @@
+----------------------------------------------------------------
+-- File : virtual_channel_demultiplexer.vhd
+-- Created :04.06.2026
+-- Author : Nico Tunkowski
+-- Project Name : HW/SW Project TM
+-- Description : CCSDS-132.0-B-3 Virtual Channel Demultiplexing Function
+----------------------------------------------------------------
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity virtual_channel_demultiplexer is
+    generic (
+        -- create this generic for every master channel
+        virtual_channel_1_id_g: std_logic_vector(2 downto 0);
+    );
+    port (
+        -- inputs
+        data_i: in std_logic_vector(7 downto 0);
+        clk_i: in std_logic;
+        data_valid_i: in std_logic;
+        reset_i: in std_logic;
+
+        virtual_channel_id_i: in std_logic_vector(2 downto 0);
+
+        -- outputs
+        -- create these outputs for every master channel
+        data_mc_1_o: out std_logic_vector(7 downto 0);
+        data_valid_mc_1_o: out std_logic
+    );
+
+end entity virtual_channel_demultiplexer;
+
+architecture behavioral of virtual_channel_demultiplexer is
+
+begin
+
+    demultiplexer: process is
+    begin
+        if reset_i = '0' then
+            data_valid_o <= 0;
+            data_mc_1_o <= x"00";
+        else
+            case virtual_channel_id_i
+                -- create this case for every master channel
+                when virtual_channel_1_id_g =>
+                    data_mc_1_o <= data_i;
+                    data_valid_mc_1_o <= data_valid_i;
+                when others =>
+                    data_valid_o <= 0;
+                    data_mc_1_o <= x"00";
+            end case;
+        end if;
+    end process demultiplexer;
+
+end architecture behavioral;
