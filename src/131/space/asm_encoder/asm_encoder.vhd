54312f8202bf8ff6dd0cd29e107d25f1e94dad17
grayandscaled <hannahlindner04@gmail.com>
2026-06-04 11:12:44 +0200

    99: back to a working version of asm

--- src/131/space/asm_encoder/asm_encoder.vhd
+++ src/131/space/asm_encoder/asm_encoder.vhd
@@ -67,7 +67,7 @@ begin
         -- reset axi signals 
         m_tvalid_r        <= '0';
         s_tready_r        <= '0';
-        
+        generate_asm_r    <= '0';
     elsif rising_edge(clk_i) then
         -- reset data valid flags 
         s_tready_r    <=  m_axi_tready;
